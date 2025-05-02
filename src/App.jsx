933485b83f145ba320fe7f0193d1681d981d5292
unknown <sc465182@gmail.com>
2025-05-02 16:44:10 +0530

    create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Education from './components/Education/Education'
+import Footer from './components/Footer/Footer'
+import Skills from './components/Skills/Skills'
+import Work from './components/Work/work'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-[#050414]'>
+     <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+     <div className='relative pt-20'>
+      <Navbar/>
+      <About/>
+      <Skills/>
+      <Work/>
+      <Education/>
+      <Contact/>
+      <Footer/>
+     </div>
+    </div>
   )
 }
 
