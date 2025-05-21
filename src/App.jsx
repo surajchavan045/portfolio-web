51ce1a5499b99226d5f85fd1e69a678d8c24db81
unknown <sc465182@gmail.com>
2025-05-21 11:39:23 +0530

    start service section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Service from './components/Service'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Service/>
     </div>
   )
 }
---
+++ src/components/Service.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div className="bg-black text-white py-20" id="about">
+      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Service
