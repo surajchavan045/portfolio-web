97f47598029f52c7a79a09f9edcd0208cf9b9bbb
unknown <sc465182@gmail.com>
2025-05-21 16:08:11 +0530

    complete project section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Service from './components/Service'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <Hero/>
       <About/>
       <Service/>
+      <Projects/>
+      <Contact/>
     </div>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa";
+
+const Contact = () => {
+  return (
+    <div className="bg-black text-white py-20" id="about">
+      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
+        <div className="flex flex-col md:flex-row items-center md:space-x-12">
+          <div className="flex-1">
+            <h3 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-4">
+              Let's Talk
+            </h3>
+            <p>
+              I'm open to discussion web development projectes or partnership
+              opportunities.
+            </p>
+            <div className="mb-4 mt-8">
+              <FaEnvelope className="inline-block text-green-400 mr-2"></FaEnvelope>
+              <a href="sc465182@gmail.com" className="hover:underline">
+                sc465182@gmail.com
+              </a>
+            </div>
+            <div className="mb-4">
+              <FaPhone className="inline-block text-green-400 mr-2"></FaPhone>
+              <span>7020069766</span>
+            </div>
+            <div className="mb-4">
+              <FaMapMarkedAlt className="inline-block text-green-400 mr-2"></FaMapMarkedAlt>
+              <span>Pune</span>
+            </div>
+          </div>
+          <div className="flex-1 w-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
